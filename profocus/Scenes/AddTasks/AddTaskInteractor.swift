12b1341ba041ce218ff93780e5fda33898262406
franklinmelo <franklinmelo18@gmail.com>
2022-04-09 15:52:52 -0300

    Implement add task view

--- profocus/Scenes/AddTasks/AddTaskInteractor.swift
+++ profocus/Scenes/AddTasks/AddTaskInteractor.swift
@@ -1,12 +1,21 @@
 import Foundation
 import UIKit
 
-protocol AddTaskInteracting {}
+protocol AddTaskInteracting {
+    var catetories: [String] { get }
+    
+    func createTask(title: String, categorieIntex: Int)
+}
 
 final class AddTaskInteractor: AddTaskInteracting {
+    var catetories: [String] = ["Reunião", "Task", "Programar"]
     private var presenter: AddTaskPresenting?
     
     init(presenter: AddTaskPresenting) {
         self.presenter = presenter
+    }
+    
+    func createTask(title: String, categorieIntex: Int) {
+        print("title: \(title)", "categorie: \(catetories[categorieIntex])")
     }
 }
--- profocus/Scenes/AddTasks/AddTaskViewController.swift
+++ profocus/Scenes/AddTasks/AddTaskViewController.swift
@@ -6,12 +6,17 @@ protocol PresentationDelegate: AnyObject {
 }
 
 
-protocol AddTaskDisplaing: AnyObject {
-    func displayShowKeyboard()
-    func displayHideKeyboard()
+protocol AddTaskDisplaing: AnyObject {}
+
+private extension AddTaskViewController.Layout {
+    struct container {
+       static var bottonContraint: NSLayoutConstraint = NSLayoutConstraint()
+    }
 }
 
 final class AddTaskViewController: UIViewController {
+    fileprivate enum Layout {}
+    
     private var interactor: AddTaskInteracting?
     weak var delegate: PresentationDelegate?
     
@@ -50,9 +55,35 @@ final class AddTaskViewController: UIViewController {
         $0.translatesAutoresizingMaskIntoConstraints = false
         $0.placeholder = "Digite o nome da tarefa"
         $0.returnKeyType = .done
+        $0.backgroundColor = .systemGray4
+        $0.layer.cornerRadius = 10
+        $0.layer.sublayerTransform = CATransform3DMakeTranslation(10, 0, 0)
         $0.delegate = self
         return $0
     }(UITextField())
+    
+    private lazy var picker: UIPickerView = {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.delegate = self
+        $0.dataSource = self
+        return $0
+    }(UIPickerView())
+    
+    private lazy var createButton: UIButton = {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.setTitle("Criar Tarefa", for: .normal)
+        $0.backgroundColor = .systemIndigo
+        $0.addTarget(self, action: #selector(didTapCreateTask), for: .touchUpInside)
+        return $0
+    }(UIButton())
+    
+    private lazy var cancelButton: UIButton = {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.setTitle("Cancelar", for: .normal)
+        $0.backgroundColor = .red.withAlphaComponent(0.75)
+        $0.addTarget(self, action: #selector(didTapCancelTask), for: .touchUpInside)
+        return $0
+    }(UIButton())
     
     init(interactor: AddTaskInteracting) {
         self.interactor = interactor
@@ -76,6 +107,7 @@ final class AddTaskViewController: UIViewController {
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
         delegate?.didDismiss()
+        NotificationCenter.default.removeObserver(self)
     }
     
     private func setupViews() {
@@ -84,6 +116,9 @@ final class AddTaskViewController: UIViewController {
         containerView.addSubview(titleLable)
         containerView.addSubview(descriptionLabel)
         containerView.addSubview(textField)
+        containerView.addSubview(picker)
+        containerView.addSubview(createButton)
+        containerView.addSubview(cancelButton)
     }
     
     private func setupConstraints() {
@@ -94,33 +129,63 @@ final class AddTaskViewController: UIViewController {
             dimmedView.leadingAnchor.constraint(equalTo: view.leadingAnchor)
         ])
         
+        Layout.container.bottonContraint = containerView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
         NSLayoutConstraint.activate([
-            containerView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0),
+            Layout.container.bottonContraint,
             containerView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
             containerView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
-            containerView.heightAnchor.constraint(equalToConstant: 300)
+            containerView.heightAnchor.constraint(equalToConstant: UIScreen.main.bounds.height / 2)
         ])
+        
         
         NSLayoutConstraint.activate([
             titleLable.topAnchor.constraint(equalTo: containerView.topAnchor, constant: 12),
-            titleLable.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: 12),
+            titleLable.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -12),
             titleLable.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 12),
         ])
         
         NSLayoutConstraint.activate([
-            descriptionLabel.topAnchor.constraint(equalTo: titleLable.bottomAnchor, constant: 12),
-            descriptionLabel.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: 12),
+            descriptionLabel.topAnchor.constraint(equalTo: titleLable.bottomAnchor, constant: 16),
+            descriptionLabel.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -12),
             descriptionLabel.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 12),
         ])
         
         NSLayoutConstraint.activate([
-            textField.topAnchor.constraint(equalTo: descriptionLabel.bottomAnchor, constant: 12),
-            textField.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: 12),
+            textField.topAnchor.constraint(equalTo: descriptionLabel.bottomAnchor, constant: 16),
+            textField.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -12),
             textField.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 12),
+            textField.heightAnchor.constraint(equalToConstant: 40)
+        ])
+        
+        NSLayoutConstraint.activate([
+            picker.topAnchor.constraint(equalTo: textField.bottomAnchor, constant: 16),
+            picker.trailingAnchor.constraint(equalTo: containerView.trailingAnchor),
+            picker.leadingAnchor.constraint(equalTo: containerView.leadingAnchor),
+            picker.heightAnchor.constraint(equalToConstant: 100)
+        ])
+        
+        NSLayoutConstraint.activate([
+            createButton.topAnchor.constraint(equalTo: picker.bottomAnchor, constant: 16),
+            createButton.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -12),
+            createButton.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 12),
+            createButton.heightAnchor.constraint(equalToConstant: 35),
+        ])
+        
+        NSLayoutConstraint.activate([
+            cancelButton.topAnchor.constraint(equalTo: createButton.bottomAnchor, constant: 16),
+            cancelButton.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -12),
+            cancelButton.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 12),
+            cancelButton.heightAnchor.constraint(equalToConstant: 35),
         ])
     }
     
-    private func configureViews() {}
+    private func configureViews() {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(closeKeyboard))
+        view.addGestureRecognizer(tapGesture)
+        
+        createButton.layer.cornerRadius = 35 / 2
+        cancelButton.layer.cornerRadius = 35 / 2
+    }
 
     @objc
     private func changeKeyboard(notification: Notification) {
@@ -128,23 +193,61 @@ final class AddTaskViewController: UIViewController {
         let keyboardScreenEndFrame = keyboardValue.cgRectValue
         
         if notification.name == UIResponder.keyboardWillHideNotification {
-            containerView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: .zero).isActive = true
+            Layout.container.bottonContraint.constant = 0
         } else {
-            containerView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -keyboardScreenEndFrame.height).isActive = true
+            Layout.container.bottonContraint.constant = -keyboardScreenEndFrame.height
         }
         
-        //Add animation
-        containerView.layoutIfNeeded()
+        UIView.animate(withDuration: 1) { [weak self] in
+            self?.containerView.layoutIfNeeded()
+        }
     }
     
+    @objc
+    private func closeKeyboard() {
+        view.endEditing(true)
+    }
+    
+    @objc
+    private func didTapCreateTask() {
+        guard let textFieldValue = textField.text else { return }
+        if !textFieldValue.isEmpty {
+            interactor?.createTask(title: textFieldValue , categorieIntex: picker.selectedRow(inComponent: 0))
+            return
+        }
+        
+        let alert = UIAlertController(title: "Ops!", message: "Insira um titulo para sua tarefa", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Ok", style: .default)
+        alert.addAction(action)
+        
+        present(alert, animated: true)
+    }
+    
+    @objc
+    private func didTapCancelTask() {
+        self.dismiss(animated: true)
+    }
 }
 
-extension AddTaskViewController: AddTaskDisplaing{
-    func displayShowKeyboard() {}
-    
-    func displayHideKeyboard() {}
-}
+extension AddTaskViewController: AddTaskDisplaing{}
 
 extension AddTaskViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+}
+
+extension AddTaskViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        1
+    }
     
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        interactor?.catetories.count ?? 0
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        interactor?.catetories[row] ?? ""
+    }
 }
